36e9d7bab1328d1e1afd958b9d12eeee028cfc72
shaikhaalslais <146442821+shaikhaalslais@users.noreply.github.com>
2024-12-08 21:01:56 +0000

    work

--- cswk/dashboard.cpp
+++ cswk/dashboard.cpp
@@ -1,11 +1,14 @@
 #include "dashboard.hpp"
+#include "overview.hpp"
+#include "pop.hpp"
+#include "Compliance.hpp"
+#include "window.hpp"
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QStackedWidget>
 #include <QLabel>
 #include <QComboBox>
 #include <QPushButton>
-#include <QFrame>
-#include <QProgressBar>
 #include <QFont>
 
 Dashboard::Dashboard(QWidget *parent) : QWidget(parent) {
@@ -14,7 +17,7 @@ Dashboard::Dashboard(QWidget *parent) : QWidget(parent) {
 
     // Header layout
     QHBoxLayout *headerLayout = new QHBoxLayout();
-    QLabel *titleLabel = new QLabel("Title");
+    QLabel *titleLabel = new QLabel("Dashboard");
     QFont titleFont;
     titleFont.setPointSize(18);
     titleFont.setBold(true);
@@ -23,18 +26,29 @@ Dashboard::Dashboard(QWidget *parent) : QWidget(parent) {
     QComboBox *languageComboBox = new QComboBox();
     languageComboBox->addItems({"English", "French", "Spanish"});
 
-    QPushButton *navLink1 = new QPushButton("page1");
-    QPushButton *navLink2 = new QPushButton("page2");
-    QPushButton *navLink3 = new QPushButton("page3");
-
+    QPushButton *navLink0 = new QPushButton("Dashboard");
+    QPushButton *navLink1 = new QPushButton("Overview");
+    QPushButton *navLink2 = new QPushButton("POP");
+    QPushButton *navLink3 = new QPushButton("Compliance");
+    QPushButton *navLink4 = new QPushButton("Dataset");
+    
     headerLayout->addWidget(titleLabel);
     headerLayout->addStretch();
+    headerLayout->addWidget(navLink0);
     headerLayout->addWidget(navLink1);
     headerLayout->addWidget(navLink2);
     headerLayout->addWidget(navLink3);
+    headerLayout->addWidget(navLink4);
     headerLayout->addWidget(languageComboBox);
 
-    // Main content layout
+    // To switch between pages
+    QStackedWidget *stackedWidget = new QStackedWidget(this);
+
+    //Dashboard page
+    QWidget *dashboardPage = new QWidget();
+    QVBoxLayout *dashboardLayout = new QVBoxLayout(dashboardPage);
+
+    // Card layout
     QGridLayout *contentLayout = new QGridLayout();
     for (int i = 0; i < 4; ++i) {
         QFrame *card = new QFrame();
@@ -50,7 +64,7 @@ Dashboard::Dashboard(QWidget *parent) : QWidget(parent) {
         cardDescription->setWordWrap(true);
 
         QProgressBar *dataRepresentation = new QProgressBar();
-        dataRepresentation->setValue(i);
+        dataRepresentation->setValue((i + 1) * 20);
 
         cardLayout->addWidget(cardTitle);
         cardLayout->addWidget(cardDescription);
@@ -58,9 +72,33 @@ Dashboard::Dashboard(QWidget *parent) : QWidget(parent) {
 
         contentLayout->addWidget(card, i / 2, i % 2);
     }
+    dashboardLayout->addLayout(contentLayout);
+    stackedWidget->addWidget(dashboardPage);   
+    
+    //Overview Page
+    OverviewPage *overviewPage = new OverviewPage();
+    stackedWidget->addWidget(overviewPage);  // Index 1
+
+    //Pop Page
+    POPPage *popPage = new POPPage();
+    stackedWidget->addWidget(popPage);  // Index 2
+
+    // Compliance Page
+    Compliance *compliancePage = new Compliance();
+    stackedWidget->addWidget(compliancePage);  // Index 3
+
+    //Dataset Page
+    PollutantWindow *pollutantWindow = new PollutantWindow();
+    stackedWidget->addWidget(pollutantWindow); 
+  
+    // Connect navigation buttons to switch pages
+    connect(navLink0, &QPushButton::clicked, [=]() { stackedWidget->setCurrentIndex(0); });
+    connect(navLink1, &QPushButton::clicked, [=]() { stackedWidget->setCurrentIndex(1); });
+    connect(navLink2, &QPushButton::clicked, [=]() { stackedWidget->setCurrentIndex(2); });
+    connect(navLink3, &QPushButton::clicked, [=]() { stackedWidget->setCurrentIndex(3); });
+    connect(navLink4, &QPushButton::clicked, [=]() { stackedWidget->setCurrentIndex(4); });
 
     // Add layouts to the main layout
     mainLayout->addLayout(headerLayout);
-    mainLayout->addLayout(contentLayout);
-    mainLayout->addStretch();
+    mainLayout->addWidget(stackedWidget);
 }
--- cswk/dashboard.hpp
+++ cswk/dashboard.hpp
@@ -1,10 +1,27 @@
 #pragma once
 
 #include <QWidget>
+#include <QPushButton>
+#include <QStackedWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QGridLayout>
+#include <QLabel>
+#include <QComboBox>
+#include <QTableView>
+#include "window.hpp"
 
 class Dashboard : public QWidget {
     Q_OBJECT
 
 public:
     explicit Dashboard(QWidget *parent = nullptr);
+
+private:
+    // Navigation buttons links
+    QPushButton *navLink0;  
+    QPushButton *navLink1;  
+    QPushButton *navLink2;  
+    QPushButton *navLink3;  
+    QPushButton *navLink4; 
 };
